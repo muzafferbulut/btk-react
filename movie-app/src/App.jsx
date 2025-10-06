d60286b5b819dc87c71d6a5d576a7b9cba073e7f
Muzaffer Bulut <bulutmuzafferr@gmail.com>
2025-10-06 11:00:37 +0300

    handling errors kısmı tamamlandı

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,96 +1,84 @@
-import { useState } from "react";
-
-const movie_list = [
-  {
-    Id: "769",
-    Title: "GoodFellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-  },
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://image.tmdb.org/t/p/original/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg",
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
-  },
-];
-
-const selected_movie_list = [
-  {
-    Id: "769",
-    Title: "GoodFellas",
-    Year: "1990",
-    Poster:
-      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-    duration: 120,
-    rating: 8.4,
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-    duration: 140,
-    rating: 8.9,
-  },
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
-    duration: 130,
-    rating: 9.1,
-  },
-];
+import { useEffect, useState } from "react";
+import get_key from "./key";
 
 const getAverage = (array) =>
-  array.reduce((sum, value) => sum + value, 0) / array.length;
+  array.reduce((sum, value) => sum + value / array.length, 0);
+
+const api_key = get_key();
+const query = "last";
 
 export default function App() {
-  const [movies, setMovies] = useState(movie_list);
-  const [selectedMovies, setSelectedMovies] = useState(selected_movie_list);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovies, setSelectedMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(function () {
+    async function getMovies() {
+      try {
+        setLoading(true);
+        setError("");
+        const res = await fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`
+        );
+
+        if (!res.ok) {
+          throw new Error("Bilinmeyen hata oluştu.");
+        }
+
+        const data = await res.json();
+        if (data.total_results === 0) {
+          throw new Error("Film bulunamadı.");
+        }
+
+        setMovies(data.results);
+      } catch (err) {
+        setError(err.message);
+      }
+      setLoading(false);
+    }
+    getMovies();
+  }, []);
 
   return (
     <>
       <Nav>
         <Logo />
         <Search />
-        <NavSearchResult movies={movies} />
+        <NavSearchResults movies={movies} />
       </Nav>
       <Main>
         <div className="row mt-2">
           <div className="col-md-9">
             <ListContainer>
-              <MovieList movies={movies} />
+              {loading && <Loading />}
+              {!loading && !error && <MovieList movies={movies} />}
+              {error && <ErrorMessage message={error} />}
             </ListContainer>
           </div>
           <div className="col-md-3">
             <ListContainer>
+              <>
                 <MyListSummary selectedMovies={selectedMovies} />
                 <MyMovieList selectedMovies={selectedMovies} />
+              </>
             </ListContainer>
           </div>
         </div>
       </Main>
     </>
+  );
+}
+
+function ErrorMessage({ message }) {
+  return <div className="alert alert-danger">{message}</div>;
+}
+
+function Loading() {
+  return (
+    <div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
   );
 }
 
@@ -104,18 +92,10 @@ function Nav({ children }) {
   );
 }
 
-function NavSearchResult({ movies }) {
-  return (
-    <div className="col-4 text-end">
-      <strong>{movies.length}</strong> kayıt bulundu.
-    </div>
-  );
-}
-
 function Logo() {
   return (
     <div className="col-4">
-      <i className="bi bi-camera-reels me-3"></i>
+      <i className="bi bi-camera-reels me-2"></i>
       Movie App
     </div>
   );
@@ -124,13 +104,21 @@ function Logo() {
 function Search() {
   return (
     <div className="col-4">
-      <input type="text" className="form-control" placeholder="Film ara.." />
+      <input type="text" className="form-control" placeholder="Film ara..." />
+    </div>
+  );
+}
+
+function NavSearchResults({ movies }) {
+  return (
+    <div className="col-4 text-end">
+      <strong>{movies.length}</strong> kayıt bulundu.
     </div>
   );
 }
 
 function Main({ children }) {
-  return <main className="container"> {children} </main>;
+  return <main className="container">{children}</main>;
 }
 
 function ListContainer({ children }) {
@@ -156,7 +144,7 @@ function MovieList({ movies }) {
   return (
     <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
       {movies.map((movie) => (
-        <Movie movie={movie} key={movie.Id} />
+        <Movie movie={movie} key={movie.id} />
       ))}
     </div>
   );
@@ -166,12 +154,21 @@ function Movie({ movie }) {
   return (
     <div className="col mb-2">
       <div className="card">
-        <img src={movie.Poster} alt={movie.Title} className="card-img-top" />
+        <img
+          src={
+            movie.poster_path
+              ? `https://media.themoviedb.org/t/p/w440_and_h660_face` +
+                movie.poster_path
+              : "/img/no-image.jpg"
+          }
+          alt={movie.title}
+          className="card-img-top"
+        />
         <div className="card-body">
-          <h6 className="card-title">{movie.Title}</h6>
+          <h6 className="card-title">{movie.title}</h6>
           <div>
-            <i className="bi bi-calendar2-date"></i>
-            <span> {movie.Year} </span>
+            <i className="bi bi-calendar2-date me-1"></i>
+            <span>{movie.release_date}</span>
           </div>
         </div>
       </div>
@@ -180,23 +177,25 @@ function Movie({ movie }) {
 }
 
 function MyListSummary({ selectedMovies }) {
-  const avgRating = getAverage(selected_movie_list.map((m) => m.rating));
-  const avgDuration = getAverage(selected_movie_list.map((m) => m.duration));
-  <div className="card mb-2">
-    <div className="card-body">
-      <h5>Listeye {selectedMovies.length} film eklendi. </h5>
-      <div className="d-flex justify-content-between">
-        <p>
-          <i className="bi bi-star-fill text-warning"></i>
-          <span>{avgRating.toFixed(2)}</span>
-        </p>
-        <p>
-          <i className="bi bi-hourglass text-warning"></i>
-          <span>{avgDuration.toFixed(0)} min.</span>
-        </p>
+  const avgRating = getAverage(selectedMovies.map((m) => m.rating));
+  const avgDuration = getAverage(selectedMovies.map((m) => m.duration));
+  return (
+    <div className="card mb-2">
+      <div className="card-body">
+        <h5>Listeye [{selectedMovies.length}] film eklendi.</h5>
+        <div className="d-flex justify-content-between">
+          <p>
+            <i className="bi bi-star-fill text-warning me-1"></i>
+            <span>{avgRating.toFixed(2)}</span>
+          </p>
+          <p>
+            <i className="bi bi-hourglass-split text-warning me-1"></i>
+            <span>{avgDuration} dk</span>
+          </p>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 function MyMovieList({ selectedMovies }) {
@@ -219,14 +218,14 @@ function MyListMovie({ movie }) {
         <div className="col-8">
           <div className="card-body">
             <h6 className="card-title">{movie.Title}</h6>
-            <div className="d-flex justify-content-between ">
+            <div className="d-flex justify-content-between">
               <p>
-                <i className="bi bi-star-fill text-warning"></i>
-                <span> {movie.rating} </span>
+                <i className="bi bi-star-fill text-warning me-1"></i>
+                <span>{movie.rating}</span>
               </p>
               <p>
-                <i className="bi bi-hourglass text-warning"></i>
-                <span> {movie.duration} min. </span>
+                <i className="bi bi-hourglass text-warning me-1"></i>
+                <span>{movie.duration} dk</span>
               </p>
             </div>
           </div>
